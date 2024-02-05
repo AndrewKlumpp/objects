a762b3e630d8b2056644a83e6402e4b08fe1c1e6
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-02-04 18:38:27 -0600

    initial commit

---
+++ printObject.js
@@ -0,0 +1,14 @@
+function printObject(obj) {
+    // your code here...
+  }
+
+  let bootcamp = {
+    name: "App Academy",
+    color: "Red",
+    population: 120
+  };
+
+  printObject(bootcamp); // prints
+  // name - App Academy
+  // color - Red
+  // population - 120
