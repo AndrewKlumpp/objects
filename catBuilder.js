e4c0164b6dbf7d6b7ac29bc9408585ebf332187a
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-02-06 10:16:04 -0600

    initial commit

---
+++ catBuilder.js
@@ -0,0 +1,9 @@
+function catBuilder(name, color, toys) {
+    // your code here...
+  }
+
+  console.log(catBuilder("Whiskers", "black", ["scratching-post", "yarn"]));
+  // prints: { name: 'Whiskers', color: 'black', toys: ['scratching-post', 'yarn'] }
+
+  console.log(catBuilder("Nyan", "rainbow", ["poptarts"]));
+  // prints: { name: 'Nyan', color: 'rainbow', toys: [ 'poptarts' ] }
